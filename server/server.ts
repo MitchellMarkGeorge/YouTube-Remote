76f18800aa2c892e5a87317b62b6d7e2ab7d1108
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2020-11-25 16:37:04 -0500

    Heroku

--- server/server.ts
+++ server/server.ts
@@ -4,12 +4,13 @@ import http from "http";
 import cors from "cors";
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 const PORT = process.env.port || 5050;
 
-app.use(cors());
+
 
 let rooms: string[] = [];
 
