a5e6f416c20ea248c7de6df320d4fe4344825978
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2020-11-25 16:47:23 -0500

    Heroku

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import http from "http";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors()); 
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
